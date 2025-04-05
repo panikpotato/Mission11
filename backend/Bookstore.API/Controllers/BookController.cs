43e7d54c6319a547b750441df4208e68781aa1d8
Jo_Jo_from_Mars <joshuayang99@gmail.com>
2025-04-05 12:10:36 -0600

    first commit

---
+++ backend/Bookstore.API/Controllers/BookController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OnlineBookstore.API.Data;
+
+namespace OnlineBookstore.API.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class BookController : Controller
+{
+    private BookDbContext _context;
+
+    public BookController(BookDbContext temp) => _context = temp;
+
+    [HttpGet]
+    public IActionResult GetBooks(int pageHowMany, int pageNum = 1)
+    {
+        var books = _context.Books.Skip((pageNum - 1) * pageHowMany).Take(pageHowMany).ToList();
+
+        var totalNumBooks = _context.Books.Count();
+        var someObject = new
+        {
+            books = books,
+            totalNumBooks = totalNumBooks
+        };
+
+        return Ok(someObject);
+    }
+
+
+
+
+}
